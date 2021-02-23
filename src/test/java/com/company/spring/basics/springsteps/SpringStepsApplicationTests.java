c92a12c4c0a576023b9daaa72934feaf22a90ad9
Stuart Smith <stuartsayswhat@googlemail.com>
2021-02-23 19:11:54 +0000

    initial

---
+++ src/test/java/com/company/spring/basics/springsteps/SpringStepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.spring.basics.springsteps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
