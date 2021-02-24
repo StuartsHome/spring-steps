a830b403c913217df123a72e123fdb329a5c8e0c
Stuart Smith <stuartsayswhat@googlemail.com>
2021-02-24 09:29:47 +0000

    added new class

---
+++ src/main/java/com/company/spring/basics/springsteps/BinarySearchImpl.java
@@ -0,0 +1,15 @@
+package com.company.spring.basics.springsteps;
+
+public class BinarySearchImpl {
+
+
+    public int binarySearch(int[] numbers, int target) {
+        // Sorting an array
+        // Search the array
+        // Return the index - result
+
+        return 3;
+
+    }
+
+}
